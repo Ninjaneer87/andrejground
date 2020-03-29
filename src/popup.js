fb37cfaebc6054b4c296be0b526d35c3aaf3a818
Andrej Forgac <komodoutd@gmail.com>
2020-03-29 11:12:09 +0200

    first commit

---
+++ src/popup.js
@@ -0,0 +1,20 @@
+export const createPopup = (htmlContent) => {
+    document.querySelector('.popup-div-content').innerHTML = htmlContent;
+    console.log(htmlContent)
+    popup('add');
+};
+
+export const popup = (action) => {
+    document.querySelector('.popup-overlay').classList[action]('show-popup');
+    document.querySelector('body').classList[action]('no-scroll');
+    if(action === 'add') {
+        document.querySelector('body').classList[action]('perspective');
+    } else {
+        setTimeout(() => {
+            document.querySelector('body').classList[action]('perspective');
+        }, 150);
+    }
+    
+    // state.popup = action === 'add'? true : false;
+    // elements.mainContainer.classList[action]('add-blur');
+}
